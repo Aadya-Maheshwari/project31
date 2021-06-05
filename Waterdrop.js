a5760877d71a7076a8d6475273b8b4a0050b58d7
Aadya-Maheshwari <76033806+Aadya-Maheshwari@users.noreply.github.com>
2021-06-05 19:30:36 +0530

    Update Waterdrop.js

--- Waterdrop.js
+++ Waterdrop.js
@@ -1,21 +1,17 @@
 class Waterdrpo{
     constructor(x,y){
        var option={
-           isStatic:false,
+           
            restitution:0.8,
            friction:0.1,
-           density:1
+         
        }
-        this.body=Bodies.circle(x,y,50,opition);
+        this.body=Bodies.circle(x,y,50,option);
         this.radius=50;
         World.add(world,this.body);
     }
     display(){
-        push()
-        var maxDrop=100;
-        for(var i=0;i<maxDrop;i++){
-            drop.push(new createDrop (random(0,400)),(random(0,400)));
-        }
-        pop()
+        ellipseMode(CENTER);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
     }
 }
